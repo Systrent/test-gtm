fb6f35eb1c2f278b51f313be18c735daba7ff2c8
Systrent <juanpams99@gmail.com>
2024-05-15 02:04:33 -0600

    feat: improved and added component for GTM test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { TestingButton } from "./components/TestingButton";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello Faable</p>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 relative bg-white">
+      <h1 className="text-5xl font-bold text-black">Test for Faable</h1>
+      <TestingButton />
     </main>
   );
 }
